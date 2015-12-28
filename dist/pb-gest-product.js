611064a8971d0d3985a14b9a77ca239b14787ba8
alessandro231 <alessandro231@gmail.com>
2015-12-28 10:51:01 +0100

    fix expiration date

--- dist/pb-gest-product.js
+++ dist/pb-gest-product.js
@@ -1,4 +1,4 @@
-/*! pb-gest-product 0.1.3 - Copyright 2015 Palmabit <hello@palmabit.com> (http://www.palmabit.com) */
+/*! pb-gest-product 0.2.0 - Copyright 2015 Palmabit <hello@palmabit.com> (http://www.palmabit.com) */
 'use strict';
 
 var _get = function get(_x, _x2, _x3) { var _again = true; _function: while (_again) { var object = _x, property = _x2, receiver = _x3; desc = parent = getter = undefined; _again = false; if (object === null) object = Function.prototype; var desc = Object.getOwnPropertyDescriptor(object, property); if (desc === undefined) { var parent = Object.getPrototypeOf(object); if (parent === null) { return undefined; } else { _x = parent; _x2 = property; _x3 = receiver; _again = true; continue _function; } } else if ('value' in desc) { return desc.value; } else { var getter = desc.get; if (getter === undefined) { return undefined; } return getter.call(receiver); } } };
@@ -93,7 +93,7 @@ var ProductPresenter = (function () {
         product.quantity = 0;
 
         product.lots.sort(function (a, b) {
-          return new Date(a.date) - new Date(b.date);
+          return new Date(a.expiration) - new Date(b.expiration);
         });
 
         for (var i = 0; i < product.lots.length; i += 1) {

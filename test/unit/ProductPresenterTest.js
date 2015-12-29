c24cf0a6869b549445b7516a296fba382b6ec18b
alessandro231 <alessandro231@gmail.com>
2015-12-29 16:22:29 +0100

    ProductPresenterTest

---
+++ test/unit/ProductPresenterTest.js
@@ -0,0 +1,71 @@
+describe("ProductPresenterTest", function () {
+  describe("Price product presenter", function () {
+    var product = {
+      prices: [
+        {list: 1, price: 1},
+        {list: 2, price: 2},
+        {list: 3, price: 3}
+      ]
+    };
+
+    it("should present product price", function () {
+      var presenter = new ProductPresenter({list: {_id: 1}});
+      var presented = presenter.getPrice(product);
+      expect(presented.price).toBe(1);
+    });
+
+    it("should present product price by default price", function () {
+      var presenter = new ProductPresenter({list_default: {_id: 2}});
+      var presented = presenter.getPrice(product);
+      expect(presented.price).toBe(2);
+    });
+
+    it("should present product price by active price", function () {
+      var presenter = new ProductPresenter({list_active: {_id: 3}});
+      var presented = presenter.getPrice(product);
+      expect(presented.price).toBe(3);
+    });
+
+    it("should present product with not found price", function () {
+      var presenter = new ProductPresenter({list: {_id: 4}});
+      var presented = presenter.getPrice(product);
+      expect(presented.price).toBe(0);
+    });
+  });
+
+  describe("Quantity product presenter", function () {
+    var product;
+
+    beforeEach(function () {
+      product = {
+        intangible: false,
+        hasLots: true,
+        lots: [
+          {lot: 'a', quantity: 10},
+          {lot: 'b', quantity: 20}
+        ],
+        noLots: {quantity: 50}
+      };
+    });
+
+    it("should get undefined quantity if product is intangible", function () {
+      product.intangible = true;
+      var presenter = new ProductPresenter();
+      var presented = presenter.getQuantity(product);
+      expect(presented.quantity).toBeUndefined();
+    });
+
+    it("should get total quantity from lots", function () {
+      var presenter = new ProductPresenter();
+      var presented = presenter.getQuantity(product);
+      expect(presented.quantity).toBe(30);
+    });
+
+    it("should get quantity if product has no lots", function () {
+      product.hasLots = false;
+      var presenter = new ProductPresenter();
+      var presented = presenter.getQuantity(product);
+      expect(presented.quantity).toBe(50);
+    });
+  });
+});
